620b45abcb90a24c1e5314c7be61138b2fe0d72b
Ben Wiedermann <benw@cs.hmc.edu>
2025-04-07 21:14:30 -0700

    Update gitignore

--- Semantics/ConcreteSemantics.hs
+++ Semantics/ConcreteSemantics.hs
@@ -1,10 +1,11 @@
 module Semantics.ConcreteSemantics where
 
-import AbstractSyntax.AST  -- To access the AST (programs, statements, line numbers, etc.)
-import Semantics.Domains   -- To access concrete stores and values
+import           AbstractSyntax.AST
+import           Semantics.Domains
+import           System.IO
 
 -----------------------------------------------------------------------------------------
--- * Concrete Domains 
+-- * Concrete Domains
 -----------------------------------------------------------------------------------------
 initialStore :: ConcreteStore
 initialStore = emptyStore
@@ -20,19 +21,19 @@ doProgram :: Program -> IO (LineNumber, ConcreteStore)
 doProgram p = loop (1, initialStore)
 
   where loop (l, σ) = case getStatement p l of
-                       
+
                         -- If this statement is Halt, we're done!
                         Halt -> return (l, σ)
-                      
-                        -- Otherwise, take a step and use the results as input to 
-                        -- the next step 
+
+                        -- Otherwise, take a step and use the results as input to
+                        -- the next step
                         _    -> doStatement p (l, σ) >>= loop
 
 -----------------------------------------------------------------------------------------
 -- * Statement transitions
 -----------------------------------------------------------------------------------------
 doStatement :: Program -> (LineNumber, ConcreteStore) -> IO (LineNumber, ConcreteStore)
-doStatement p (l, σ) = 
+doStatement p (l, σ) =
 
   case getStatement p l of
 
@@ -45,12 +46,11 @@ doStatement p (l, σ) =
     (Print x) -> do print (resolve x σ)
                     return (l + 1, σ)
 
-    (Input x) -> do putStr "type a number: "
-                    n <- read <$> getLine
+    (Input x) -> do n <- prompt "type a number: "
                     return (l+1, σ // [x ↦ n])
 
     (Assign     x t) -> return (l+1, σ // [x ↦ evalTerm t σ])
-    
+
     (AssignExpr x e) -> return (l+1, σ // [x ↦ evalExpr e σ])
 
     (If c l') -> if evalCond c σ
@@ -79,3 +79,15 @@ evalExpr (AE term1 Divide term2) σ = evalTerm term1 σ `div` evalTerm term2 σ
 evalCond :: Condition -> ConcreteStore -> Bool
 evalCond (Cond x Equal t)    σ = resolve x σ == evalTerm t σ
 evalCond (Cond x LessThan t) σ = resolve x σ <  evalTerm t σ
+
+
+-----------------------------------------------------------------------------------------
+-- * Helper functions
+-----------------------------------------------------------------------------------------
+
+-- From: https://stackoverflow.com/questions/13190314/io-happens-out-of-order-when-using-getline-and-putstr
+prompt :: String -> IO Integer
+prompt text = do
+    putStr text
+    hFlush stdout
+    read <$> getLine
